4b7d3e999af2beecdc7952d82963010b91c6e17b
Gun Park <mujjingun@gmail.com>
2020-04-10 01:26:51 +0900

    add temp.js

---
+++ temp.js
@@ -0,0 +1,38 @@
+
+var express = require('express');
+var app = express();
+
+mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'me',
+    password: 'mypassword',
+    database: 'mydb'
+})
+connection.connect();
+
+function insert_sensor(device, unit, type, value, seq, ip) {
+  obj = {};
+  obj.seq = seq;
+  obj.device = device;
+  obj.unit = unit;
+  obj.type = type;
+  obj.value = value;
+  obj.ip = ip.replace(/^.*:/, '')
+
+  var query = connection.query('insert into sensors set ?', obj, function(err, rows, cols) {
+    if (err) throw err;
+    console.log("database insertion ok= %j", obj);
+  });
+}
+
+app.get('/', function(req, res) {
+    var r = req.query;
+    res.json("hello");
+});
+
+var server = app.listen(8098, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log('listening at http://%s:%s', host, port)
+});
